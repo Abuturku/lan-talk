5a450ff1accada6a28837097180cb6c1f5ec9f43
Niklas Nikisch <n.nikisch@gmail.com>
2017-05-23 10:20:28 +0200

    Sync fehler behoben

--- src/main/java/dhbw/lan/lantalk/persistence/objects/User.java
+++ src/main/java/dhbw/lan/lantalk/persistence/objects/User.java
@@ -65,7 +65,8 @@ public class User implements IPrimKey {
 	/**
 	 * Represents a list of comments the user has written
 	 */
-	@OneToMany(mappedBy = "user", targetEntity = TextComponent.class, cascade = { CascadeType.PERSIST })
+	@OneToMany(mappedBy = "user", targetEntity = TextComponent.class, cascade = {
+			CascadeType.PERSIST }, fetch = FetchType.EAGER)
 	private List<Comment> commentList;
 
 	/**
