8046e6a3d7a6cddc89cc7436a2ffe92e2ee50208
Linda <linda.latreider@web.de>
2017-05-28 21:31:46 +0200

    prepared create new post presentation side

---
+++ src/main/resources/SQL/testPostAndComment.sql
@@ -0,0 +1,11 @@
+
+insert into talk.textcomponent values (123, "test text lorem ipsum", '1495741151337', 2, 1);
+
+insert into talk.textcomponent values (137, "test talk2 lorem ipsum", '1495741157337', 15, 1);
+
+insert into talk.textcomponent values (100, "test comment lorem ipsum", '1495741181337', -2, 1);
+
+insert into talk.posts values (123);
+insert into talk.posts values (137);
+
+insert into talk.comments values (100, 123);
