bd54e29bc0dba85b50be36344fc3d1f2785eb0bf
Niklas Nikisch <n.nikisch@gmail.com>
2017-06-24 18:21:50 +0200

    ReportType fromString method

--- src/main/java/dhbw/lan/lantalk/persistence/objects/ReportType.java
+++ src/main/java/dhbw/lan/lantalk/persistence/objects/ReportType.java
@@ -2,4 +2,17 @@ package dhbw.lan.lantalk.persistence.objects;
 
 public enum ReportType {
 	Spam, Insult, Inappropriate;
+
+	public static ReportType fromString(String value) {
+		switch (value.toLowerCase()) {
+		case "spam":
+			return Spam;
+		case "insult":
+			return Insult;
+		case "inappropriate":
+			return Inappropriate;
+		default:
+			return null;
+		}
+	}
 }
